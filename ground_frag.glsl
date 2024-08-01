fe1877160038fe766ec220ec20457df9e3dfb32c
Bridget Hartin Pasco <isaacpasco@icloud.com>
2024-08-01 12:43:57 -0700

    bruh

--- ground_frag.glsl
+++ ground_frag.glsl
@@ -15,22 +15,22 @@ float noise(vec2 location){
 	} else if(tout<0.0){
 		return 0.0;
 	}
-	return tout;
+	return tout*tout;
 }
 float randv2(vec2 location){
 	return sin(round(location.x*75319.1230))+sin(round(location.y*1029935.1320));
 }
 void main(){
 	float scl = 8;
-	float v = ((noise(fragTexCoord*64*scl)/4.0+noise(fragTexCoord*32*scl)/4.0)+noise(fragTexCoord*8*scl)/4.0*2.0)*1.0;
+	float v = ((noise(fragTexCoord*16*scl)/4.0+noise(fragTexCoord*8*scl)/4.0)+noise(fragTexCoord*4*scl)/4.0*2.0)*0.8;
 	v *= v;
 	finalColor.a = 1.0;
 	finalColor.r = 0;
 	finalColor.b = 0;
 	finalColor.g = 0;
-	if(v>0.5){
-		finalColor.g = 0.6;
-	} else if(v>0.1){
+	if(v>0.2){
+		finalColor.g = 0.4;
+	} else if(v>0.05){
 		finalColor.g = 0.3;
 	} else{
 		finalColor.r = 0.2;
